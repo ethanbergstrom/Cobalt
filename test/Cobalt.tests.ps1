217e217f4f109ffc91b501c84189edd2a4ef5a65
ethanbergstrom <ethan.bergstrom@gmail.com>
2022-02-02 20:11:54 -0600

    Syntax error

--- test/Cobalt.tests.ps1
+++ test/Cobalt.tests.ps1
@@ -82,7 +82,7 @@ Describe "package upgrade" {
 		}
 
 		It 'upgrades all packages without erroring' {
-			{Update-WinGetPackage -All} Should -Not -Throw | Where-Object {$_.ID -eq $package} | Should -Not -BeNullOrEmpty
+			{Update-WinGetPackage -All} Should -Not -Throw 
 		}
 
 		It 'successfully upgraded CPU-Z a newer version' {
